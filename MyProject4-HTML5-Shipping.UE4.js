25e26beb05b1a734db4c85f60d583a3f70c3b928
Ilyaz02335 <147247680+Ilyaz02335@users.noreply.github.com>
2023-11-27 18:22:11 +0700

    Update MyProject4-HTML5-Shipping.UE4.js

--- MyProject4-HTML5-Shipping.UE4.js
+++ MyProject4-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('MyProject4-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Ilyaz02335/GamePyrobyte/a67f0101d7a9e416440a1341c170bcc0401e0814/MyProject4-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
