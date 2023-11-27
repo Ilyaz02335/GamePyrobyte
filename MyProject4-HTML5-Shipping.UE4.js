b5ff301bdbd87859d53dddcff01d2288e895b76d
Ilyaz02335 <147247680+Ilyaz02335@users.noreply.github.com>
2023-11-27 19:30:48 +0700

    Update MyProject4-HTML5-Shipping.UE4.js

--- MyProject4-HTML5-Shipping.UE4.js
+++ MyProject4-HTML5-Shipping.UE4.js
@@ -1167,9 +1167,9 @@ $(document).ready(function() {
 			});
 */
 // Instead as a fallback, download as ArrayBuffer. (TODO: Figure out the bugs with the above, and switch to using that one instead)
-			fetchOrDownloadAndStore(db, Module.locateFile('https://dl.dropboxusercontent.com/scl/fi/6wjxa7ieyqqb0kga6foyq/MyProject4-HTML5-Shipping.data?rlkey=lw258mxbez9ksobz7p7x820ui&dl=0'), 'arraybuffer').then(function(dataArrayBuffer) {
+			fetchOrDownloadAndStore(db, Module.locateFile('https://dl.dropboxusercontent.com/scl/fi/jnjd3fj5idlnrcnf1of9c/MyProject4-HTML5-Shipping.data?rlkey=em8zo6blrh4l7n3ya98npc20e&dl=0'), 'arraybuffer').then(function(dataArrayBuffer) {
 				Module['preloadedPackages'] = {};
-				Module['preloadedPackages'][Module.locateFile('https://dl.dropboxusercontent.com/scl/fi/6wjxa7ieyqqb0kga6foyq/MyProject4-HTML5-Shipping.data?rlkey=lw258mxbez9ksobz7p7x820ui&dl=0')] = dataArrayBuffer;
+				Module['preloadedPackages'][Module.locateFile('https://dl.dropboxusercontent.com/scl/fi/jnjd3fj5idlnrcnf1of9c/MyProject4-HTML5-Shipping.data?rlkey=em8zo6blrh4l7n3ya98npc20e&dl=0')] = dataArrayBuffer;
 				return dataJsDownload.then(addScriptToDom);
 			});
 
